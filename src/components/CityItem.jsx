613b375800792dae81f40a8d48b4c75790ae953d
stian røgeberg <srr007@hotmail.com>
2024-09-25 23:52:27 +0200

    Implemented country list component.

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,4 +1,5 @@
 import styles from "./CityItem.module.css";
+import PropTypes from "prop-types";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -7,14 +8,16 @@ const formatDate = (date) =>
     year: "numeric",
   }).format(new Date(date));
 
-function CityItem({ city }) {
-  const { cityName, emoji, date } = city;
+CityItem.propTypes = {
+  city: PropTypes.object,
+};
 
+function CityItem({ city }) {
   return (
     <li className={styles.cityItem}>
-      <span className={styles.emoji}>{emoji}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>({formatDate(date)})</time>
+      <span className={styles.emoji}>{city.emoji}</span>
+      <h3 className={styles.name}>{city.cityName}</h3>
+      <time className={styles.date}>({formatDate(city.date)})</time>
       <button className={styles.deleteBtn}>&times;</button>
     </li>
   );
