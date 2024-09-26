59b62c4585d5d0e1376c8c2924382d25d26980d6
stian røgeberg <srr007@hotmail.com>
2024-09-26 10:35:53 +0200

    Reading and setting a query string

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -13,6 +13,10 @@ function City() {
   // get data from URL cities/:id
   const { id } = useParams();
 
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
   // TEMP DATA
   const currentCity = {
     cityName: "Lisbon",
@@ -22,7 +26,14 @@ function City() {
   };
 
   const { cityName, emoji, date, notes } = currentCity;
-  return <h1>City {id}</h1>;
+  return (
+    <>
+      <h1>City {id}</h1>;
+      <p>
+        Position: {lat}, {lng}
+      </p>
+    </>
+  );
 }
 
 // return (
--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -14,11 +14,17 @@ CityItem.propTypes = {
 };
 
 function CityItem({ city }) {
-  const { cityName, emoji, date, id } = city;
+  const {
+    cityName,
+    emoji,
+    date,
+    id,
+    position: { lat, lng },
+  } = city;
 
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}`}>
+      <Link className={styles.cityItem} to={`${id}?lat=${lat}&lng=${lng}`}>
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
