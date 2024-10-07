c29d7fe680e761d83a3a94b6a6d7a37f617ad30b
stian røgeberg <srr007@hotmail.com>
2024-10-07 10:40:54 +0200

    Using a new button, getting the ID for the city and using the spinner

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
 import PropTypes from "prop-types";
+import { useCities } from "../context/CitiesContext";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -14,6 +15,7 @@ CityItem.propTypes = {
 };
 
 function CityItem({ city }) {
+  const { currentCity } = useCities();
   const {
     cityName,
     emoji,
@@ -24,7 +26,12 @@ function CityItem({ city }) {
 
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}?lat=${lat}&lng=${lng}`}>
+      <Link
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
+        to={`${id}?lat=${lat}&lng=${lng}`}
+      >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
--- src/context/CitiesContext.jsx
+++ src/context/CitiesContext.jsx
@@ -13,6 +13,7 @@ const CitiesContext = createContext();
 const CitiesProvider = ({ children }) => {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({}); // this state is global - needed by multiple components.
 
   useEffect(() => {
     const fetchCities = async () => {
@@ -30,11 +31,26 @@ const CitiesProvider = ({ children }) => {
     fetchCities();
   }, []);
 
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch {
+      alert("There was an error");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
         cities,
         isLoading,
+        currentCity,
+        getCity,
       }}
     >
       {children}
